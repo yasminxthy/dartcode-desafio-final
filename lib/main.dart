551a1491a45876dbc5594c7cb1b92a9cdd3d27ec
yasminxthy <castroyasmin095@gmail.com>
2026-09-25 12:28:51 -0400

    Commit 2: criação da classe Usuario

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,15 @@
+class Usuario {
+  String nome;
+  String apelido;
+  bool online;
+
+  Usuario({
+    required this.nome,
+    required this.apelido,
+    required this.online,
+  });
+}
+
 void main() {
   print('DARTCORD');
 }
